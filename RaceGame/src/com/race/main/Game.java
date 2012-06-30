99430245ed3ed1e725e040e77eab78bd54551b13
Elmar van Rijnswou <elmarvanrijnswou@hotmail.com>
2012-06-30 05:14:07 +0200

    Added background

--- RaceGame/src/com/race/main/Game.java
+++ RaceGame/src/com/race/main/Game.java
@@ -3,29 +3,43 @@ package com.race.main;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 import com.race.entity.Player;
+import com.race.system.Camera;
 
 public class Game extends BasicGame {
 	Player player;
+	Image background;
 
 	public Game() {
+		// Title
 		super("Airplanes");
 	}
 
-	@Override
-	public void render(GameContainer container, Graphics g)
-			throws SlickException {
-		player.draw(g);
-	}
-
-	@Override
+	/**
+	 * All the images sould be loaded in this function.
+	 */
 	public void init(GameContainer container) throws SlickException {
 		player = new Player();
+		background = new Image("/data/land.jpeg");
 	}
 
-	@Override
+	/**
+	 * Everything should be rendered in here.
+	 */
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		Camera.getCamera().setFocus(g);
+		background.draw(0, 0);
+		player.draw(g);
+		Camera.getCamera().releaseFocus(g);
+	}
+
+	/**
+	 * Updates everything, all entities and the buttonhandler.
+	 */
 	public void update(GameContainer gc, int deltaT) throws SlickException {
 		player.update(deltaT);
 		ButtonHandler.getButtonHandler().update(gc);
